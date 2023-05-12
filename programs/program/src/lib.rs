248a08c454939e2033cc2ddfc6f35014a2e5bf90
Syed Aabis Akhtar <74103155+GitBolt@users.noreply.github.com>
2023-05-13 01:46:40 +0530

    Add: thread id in account and withdraw method

--- programs/program/src/lib.rs
+++ programs/program/src/lib.rs
@@ -1,22 +1,27 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::{self};
 use anchor_lang::solana_program::{
     instruction::Instruction, native_token::LAMPORTS_PER_SOL, system_program,
 };
 use anchor_lang::InstructionData;
 use clockwork_sdk::state::{Thread, ThreadAccount};
 
-declare_id!("611igfTen8mrQpsKDeipSu5G885VXK1xmotvrAEj6r5V");
+declare_id!("5mP16ymxF7Ac2hw85oAzCJUUnu9deUvYTyWhaQ4M7H39");
 
-const ANNUAL_INTEREST: f64 = 100.0;
+const ANNUAL_INTEREST: f64 = 2.5; // 2.5% interest return
 const CRON_SCHEDULE: &str = "*/10 * * * * * *"; // 10s https://crontab.guru/
+const AUTOMATION_FEE: f64 = 0.05;
+
+pub const BANK_ACCOUNT_SEED: &[u8] = b"bank_account";
+pub const THREAD_AUTHORITY_SEED: &[u8] = b"authority";
 
 fn calculate_balance(created_at: i64, current_balance: f64) -> f64 {
     let now = Clock::get().unwrap().unix_timestamp;
     let elapsed_seconds = now - created_at;
 
-    let interest_per_second = ANNUAL_INTEREST / 31536000.0 as f64; // number of seconds in a year
+    let interest_per_second = ANNUAL_INTEREST / 60.0 as f64; // number of seconds in a minute
     let interest_earned = current_balance as f64 * elapsed_seconds as f64 * interest_per_second;
-    current_balance + interest_earned
+    return current_balance + interest_earned;
 }
 
 #[program]
@@ -38,6 +43,7 @@ pub mod bank {
         let bank_account = &mut ctx.accounts.bank_account;
 
         // Assigning init data
+        bank_account.thread_id = thread_id.clone();
         bank_account.holder = *holder.key;
         bank_account.balance = balance;
         bank_account.holder_name = holder_name;
@@ -50,11 +56,13 @@ pub mod bank {
                 bank_account: bank_account.key(),
                 thread: thread.key(),
                 thread_authority: thread_authority.key(),
-            }.to_account_metas(Some(true)),
+            }
+            .to_account_metas(Some(true)),
             data: crate::instruction::UpdateBalance {
                 _thread_id: thread_id.clone(),
                 new_balance: calculate_balance(bank_account.created_at, bank_account.balance),
-            }.data(),
+            }
+            .data(),
         };
 
         // Clockwork Trigger
@@ -76,7 +84,7 @@ pub mod bank {
                 },
                 &[&[THREAD_AUTHORITY_SEED, &[bump]]],
             ),
-            0.05 as u64 * LAMPORTS_PER_SOL, // https://docs.clockwork.xyz/developers/threads/fees
+            AUTOMATION_FEE as u64 * LAMPORTS_PER_SOL, // https://docs.clockwork.xyz/developers/threads/fees
             thread_id,
             vec![target_ix.into()],
             trigger,
@@ -92,6 +100,21 @@ pub mod bank {
     ) -> Result<()> {
         let bank_account = &mut ctx.accounts.bank_account;
         bank_account.balance += new_balance;
+        bank_account.updated_at = Clock::get().unwrap().unix_timestamp;
+        msg!(
+            "New Balance: {}, Updated_at: {}",
+            bank_account.balance,
+            bank_account.updated_at
+        );
+        Ok(())
+    }
+
+    pub fn withdraw(
+        ctx: Context<UpdateBalance>,
+        _thread_id: Vec<u8>,
+    ) -> Result<()> {
+        let bank_account = &mut ctx.accounts.bank_account;
+        bank_account.balance = 0.0;
         bank_account.updated_at = Clock::get().unwrap().unix_timestamp;
         msg!(
             "New Balance: {}, Updated_at: {}",
@@ -121,34 +144,10 @@ pub mod bank {
         ))?;
         Ok(())
     }
-}
 
-#[account]
-#[derive(Default)]
-pub struct BankAccount {
-    pub holder: Pubkey,
-    pub holder_name: String,
-    pub balance: f64,
-    pub created_at: i64,
-    pub updated_at: i64,
-    pub bump: u8,
-}
-
-pub const BANK_ACCOUNT_SEED: &[u8] = b"bank_account";
-/// Seed for thread_authority PDA.
-pub const THREAD_AUTHORITY_SEED: &[u8] = b"authority";
-
-#[derive(Accounts)]
-#[instruction(_thread_id: Vec<u8>)]
-pub struct UpdateBalance<'info> {
-    #[account(mut, seeds = [BANK_ACCOUNT_SEED, _thread_id.as_ref()], bump)]
-    pub bank_account: Account<'info, BankAccount>,
-
-    #[account(signer, constraint = thread.authority.eq(&thread_authority.key()))]
-    pub thread: Account<'info, Thread>,
-
-    #[account(seeds = [THREAD_AUTHORITY_SEED], bump)]
-    pub thread_authority: SystemAccount<'info>,
+    pub fn delete(_ctx: Context<DeleteAccount>, _thread_id: Vec<u8>) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -185,6 +184,40 @@ pub struct Initialize<'info> {
 }
 
 #[derive(Accounts)]
+#[instruction(_thread_id: Vec<u8>)]
+pub struct UpdateBalance<'info> {
+    #[account(mut, seeds = [BANK_ACCOUNT_SEED, _thread_id.as_ref()], bump)]
+    pub bank_account: Account<'info, BankAccount>,
+
+    #[account(signer, constraint = thread.authority.eq(&thread_authority.key()))]
+    pub thread: Account<'info, Thread>,
+
+    #[account(seeds = [THREAD_AUTHORITY_SEED], bump)]
+    pub thread_authority: SystemAccount<'info>,
+}
+
+#[derive(Accounts)]
+#[instruction(_thread_id : String)]
+pub struct DeleteAccount<'info> {
+    #[account(mut)]
+    pub holder: Signer<'info>,
+
+    #[account(
+        mut,
+        close = holder,
+        seeds = [BANK_ACCOUNT_SEED, _thread_id.as_ref()],
+        bump=bank_account.bump
+    )]
+    pub bank_account: Account<'info, BankAccount>,
+
+    // Misc Accounts
+    #[account(address = system_program::ID)]
+    pub system_program: Program<'info, System>,
+    #[account(address = solana_program::sysvar::rent::ID)]
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
 pub struct Reset<'info> {
     /// The signer.
     #[account(mut)]
@@ -210,4 +243,16 @@ pub struct Reset<'info> {
         close = holder
     )]
     pub bank_account: Account<'info, BankAccount>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct BankAccount {
+    pub holder: Pubkey,
+    pub holder_name: String,
+    pub balance: f64,
+    pub created_at: i64,
+    pub updated_at: i64,
+    pub thread_id: Vec<u8>,
+    pub bump: u8,
 }
